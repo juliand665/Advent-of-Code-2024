5371bc1656618a30db963c5e4ae1d3838bc86488
Julian Dunskus <j_dunskus@apple.com>
2024-12-02 06:38:09 +0100

    day 2

---
+++ Day 02/main.swift
@@ -0,0 +1,20 @@
+import AoC_Helpers
+import Foundation
+
+func isSafe(report: some Sequence<Int>) -> Bool {
+    let diffs = report.adjacentPairs().map { $1 - $0 }
+    return false
+    || diffs.allSatisfy { (1...3).contains(+$0) } // all gradually increasing
+    || diffs.allSatisfy { (1...3).contains(-$0) } // all gradually decreasing
+}
+
+let reports = input().lines().map { $0.ints() }
+let safeCount = reports.count(where: isSafe)
+print(safeCount)
+
+let safeCountWithSkips = reports.count { report in
+    report.indices.contains { skippedIndex in
+        isSafe(report: report.prefix(upTo: skippedIndex) + report.suffix(from: skippedIndex + 1))
+    }
+}
+print(safeCountWithSkips)
