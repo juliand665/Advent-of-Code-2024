7d919237cf921efc56a8fed10c87c62547938cb4
Julian Dunskus <j_dunskus@apple.com>
2024-12-08 00:25:02 +0100

    measure time, add part 1 flag

--- Day 07/main.swift
+++ Day 07/main.swift
@@ -10,25 +10,34 @@ let equations = input().lines().map {
     return Equation(result: ints.first!, operands: Array(ints.dropFirst()))
 }
 
-func canReach(_ result: Int, operands: ArraySlice<Int>) -> Bool {
-    let last = operands.last!
-    guard operands.count > 1 else {
-        return last == result
-    }
-    let (quotient, remainder) = result.quotientAndRemainder(dividingBy: last)
-    if remainder == 0, canReach(quotient, operands: operands.dropLast()) {
-        return true
-    } else if canReach(result - last, operands: operands.dropLast()) {
-        return true
-    } else {
-        // wanna avoid string conversions if we can help it so i've put this last
-        let resultString = String(result)
-        let lastString = String(last)
-        if resultString.ends(with: lastString) {
-            return canReach(Int(resultString.dropLast(lastString.count)) ?? 0, operands: operands.dropLast())
+for isPart2 in [false, true] {
+    func canReach(_ result: Int, operands: ArraySlice<Int>) -> Bool {
+        let last = operands.last!
+        guard operands.count > 1 else {
+            return last == result
         }
+        let (quotient, remainder) = result.quotientAndRemainder(dividingBy: last)
+        if remainder == 0, canReach(quotient, operands: operands.dropLast()) {
+            return true
+        } else if canReach(result - last, operands: operands.dropLast()) {
+            return true
+        } else if isPart2 {
+            // wanna avoid string conversions if we can help it so i've put this last
+            let resultString = String(result)
+            let lastString = String(last)
+            if resultString.ends(with: lastString) {
+                guard let prefix = Int(resultString.dropLast(lastString.count)) else { return false }
+                return canReach(prefix, operands: operands.dropLast())
+            }
+        }
+        return false
     }
-    return false
+    
+    let sum = measureTime { equations
+        .lazy
+        .filter { canReach($0.result, operands: $0.operands[...]) }
+        .map(\.result)
+        .sum()
+    }
+    print(sum)
 }
-
-print(equations.filter { canReach($0.result, operands: $0.operands[...]) }.map(\.result).sum())
