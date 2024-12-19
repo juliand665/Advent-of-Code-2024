1cbbaaf80d4f1e4ce313bb4004c2b2e0a7d26c6d
Julian Dunskus <j_dunskus@apple.com>
2024-12-19 16:56:11 +0100

    day 19
    i was way overcomplicating it because i thought it couldn't be this easy lol

---
+++ Day 19/main.swift
@@ -0,0 +1,25 @@
+import AoC_Helpers
+import Algorithms
+import HandyOperators
+
+let (towels, patterns) = input().lineGroups().splat { towels, patterns in
+    (
+        towels.onlyElement()!.components(separatedBy: ", ").sorted(),
+        patterns.map(String.init)
+    )
+}
+
+var memo: [Substring: Int] = [:]
+func waysToMake(_ pattern: Substring) -> Int {
+    guard !pattern.isEmpty else { return 1 }
+    if let ways = memo[pattern] { return ways }
+    return towels
+        .lazy
+        .filter(pattern.hasSuffix(_:))
+        .sum { waysToMake(pattern.dropLast($0.count)) }
+    <- { memo[pattern] = $0 }
+}
+
+let ways = measureTime { towels.map { waysToMake($0[...]) } }
+print(ways.count { $0 > 0 })
+print(ways.sum())
